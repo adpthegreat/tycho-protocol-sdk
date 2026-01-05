55de0ef35b93dd882e8c3d41942309d172887fbb
adpthegreat <adpthegreatanddefiant@gmail.com>
2026-01-05 07:18:57 +0100

    feat:added entrypoints to fields, updated ethereum-uniswap.yaml

--- substreams/ethereum-uniswap-v2/src/modules/5_map_pool_events.rs
+++ substreams/ethereum-uniswap-v2/src/modules/5_map_pool_events.rs
@@ -9,7 +9,8 @@ use tycho_substreams::{
     models::entry_point_params::TraceData,
     prelude::*,
 };
-use substreams::prelude::StoreGetBigInt;
+use crate::abi::{self};
+use substreams::prelude::{StoreGetBigInt, BigInt};
 
 // Auxiliary struct to serve as a key for the HashMaps.
 #[derive(Clone, Hash, Eq, PartialEq)]
@@ -33,7 +34,7 @@ struct PartialChanges {
     // entrypoints: HashSet<EntryPoint>,
     // entrypoint_params: HashSet<EntryPointParams>,
 }
-//refactor to use Protocol Changes One day 
+
 impl PartialChanges {
     // Consolidate the entity changes into a vector of EntityChanges. Initially, the entity changes
     // are in a map to prevent duplicates. For each transaction, we need to have only one final
@@ -57,47 +58,47 @@ pub fn map_pool_events(
     block: eth::Block,
     block_entity_changes: BlockChanges,
     pools_store: StoreGetProto<ProtocolComponent>,
-    balance_store:
-    // balance_deltas: BlockBalanceDeltas 
+    balance_deltas: BlockBalanceDeltas 
 ) -> Result<BlockChanges, substreams::errors::Error> {
     // Sync event is sufficient for our use-case. Since it's emitted on every reserve-altering
     // function call, we can use it as the only event to update the reserves of a pool.
     let mut block_entity_changes = block_entity_changes;
     let mut tx_changes: HashMap<Vec<u8>, PartialChanges> = HashMap::new();
-    //Basically, the operation is as so ->
-    //Get all the balance deltas from a tx 
-    //accumulate the balance deltas for a tx 
-    //assign 
+
+    // Get all the balance deltas from a tx 
+    // accumulate the balance deltas for a tx
+    // assign them 
+    //since there are multiple txs in a block we have to emit the right balance changes per tx
+    //with the reserve amount we will do this using the ordinal 
 
     // // Build a map of cumulative balances per transaction so the right balance can be emitted 
     // // Key: "pool_id:token" -> Vec of (ord, cumulative_balance)
-    // let mut balance_map: HashMap<String, Vec<(u64, BigInt)>> = HashMap::new();
+    let mut balances_map: HashMap<String, Vec<(u64, BigInt)>> = HashMap::new();
 
-    // // Sort deltas by ord to process them in transaction order
-    // let mut sorted_deltas = balance_deltas.balance_deltas.clone();
-    // sorted_deltas.sort_by_key(|d| d.ord);
+    // Sort deltas by ord to process them in transaction order
+    let mut sorted_deltas = balance_deltas.balance_deltas.clone();
+    sorted_deltas.sort_by_key(|d| d.ord);
 
-    // // Build cumulative balances
-    // for delta in sorted_deltas {
-    //     let component_id = String::from_utf8(delta.component_id.clone())
-    //         .expect("component_id is not valid utf-8");
-    //     let key = format!("{}:{}", component_id, hex::encode(&delta.token));
+    // Build cumulative balances in balances_map
+    for delta in sorted_deltas {
+        let component_id = String::from_utf8(delta.component_id.clone())
+            .expect("component_id is not valid utf-8");
+        let key = format!("{}:{}", component_id, hex::encode(&delta.token));
         
-    //     let delta_value = BigInt::from_signed_bytes_be(&delta.delta);
+        let delta_value = BigInt::from_signed_bytes_be(&delta.delta);
         
-    //     balance_map
-    //         .entry(key.clone())
-    //         .and_modify(|entries| {
-    //             let last_balance = entries.last().map(|(_, bal)| bal.clone()).unwrap_or(BigInt::zero());
-    //             let new_balance = last_balance + delta_value.clone();
-    //             entries.push((delta.ord, new_balance));
-    //         })
-    //         .or_insert_with(|| vec![(delta.ord, delta_value)]);
-    // }
+        balances_map
+            .entry(key.clone())
+            .and_modify(|entries| {
+                let last_balance = entries.last().map(|(_, bal)| bal.clone()).unwrap_or(BigInt::zero());
+                let new_balance = last_balance + delta_value.clone();
+                entries.push((delta.ord, new_balance));
+            })
+            .or_insert_with(|| vec![(delta.ord, delta_value)]);
+    }
 
     handle_sync(&block, &mut tx_changes, &pools_store);
-    handle_transfers(&block, &mut tx_changes, &pools_store, balance_store);
-    // handle_transfers(&block, &mut tx_changes, &pools_store, balance_map);
+    handle_transfers(&block, &mut tx_changes, &pools_store, &mut balances_map);
 
     merge_block(&mut tx_changes, &mut block_entity_changes);
 
@@ -151,7 +152,7 @@ fn handle_sync(
             );
         }
 
-        // Update balance changes for each token
+        // Update balance changes for each token (create balance changes for only token_a and token_b)
         for (index, token) in pool.tokens[..2].iter().enumerate() { 
             let balance = &reserves_bytes[index];
             // HashMap also prevents having duplicate balance changes for the same pool and token.
@@ -182,29 +183,15 @@ fn handle_transfers(
     block:&eth::Block,
     tx_changes: &mut HashMap<Vec<u8>, PartialChanges>,
     store: &StoreGetProto<ProtocolComponent>,
-    balance_store: StoreGetBigInt
+    balances_map: &mut HashMap<String, Vec<(u64, BigInt)>>,
 ) {
-    //filters transfers by pool address so that means it'll only filter txs that have an LP Token transfer (LP Token transfer is the same)
-    //and thats the only time it'll update the balances and we'll see balance0 and balance1 
-    //see pool 
-    //get the last balance -> update the last balance
-    //add to entity changes 
-
-    //issue with this is that interim txs in the block will register the final balance 
-    //of the pool at the end of the block, with the actual reserves at that point 
-    //which is an actual issue 
     let mut on_transfer = |event: Transfer, tx: &eth::TransactionTrace, log: &eth::Log| {
         let pool_address = &log.address;
-        //when i used pool_address_hex as 
-        // let pool_address_hex = pool_address.clone().to_hex();
-
-        //the problem with this if there are multiple balance changes to a pool in the same block
-        //it registers the same thing as the latest change 
-
         //some balances are the same as the reserve , some are not 
-         // Check if either 'to' or 'from' address is a pool
+        // Check if either 'to' or 'from' address is a pool
         let to_hex = event.to.to_hex();
         let from_hex = event.from.to_hex();
+        let tx_ord = tx.end_ordinal;
 
         let pool = [&to_hex, &from_hex]
             .iter()
@@ -216,67 +203,29 @@ fn handle_transfers(
 
             let token0 = &pool.tokens[0];
             let token1 = &pool.tokens[1];
-
-            // // Helper function to get balance at specific ordinal
-            //     let get_balance_at_ord = |key: &str, ord: u64| -> Option<BigInt> {
-            //         balance_map.get(key).and_then(|entries| {
-            //             // Find the last entry with ord <= tx_ord
-            //             entries
-            //                 .iter()
-            //                 .rev()
-            //                 .find(|(entry_ord, _)| *entry_ord <= ord)
-            //                 .map(|(_, balance)| balance.clone())
-            //         })
-            //     };
-
-            //     let key_token0 = format!("{}:{}", pool_id, hex::encode(&token0));
-            //     let key_token1 = format!("{}:{}", pool_id, hex::encode(&token1));
-            //     let key_liquidity = format!("{}:{}", pool_id, pool_id);
-
-            //     let token0_balance = get_balance_at_ord(&key_token0, tx_ord);
-            //     let token1_balance = get_balance_at_ord(&key_token1, tx_ord);
-            //     let liquidity_balance = get_balance_at_ord(&key_liquidity, tx_ord);
             //pool id is the string version of the address with "0x"
             //balance delta component id does not have "0x"
             let pool_id = pool.id.trim_start_matches("0x");
 
-            let token0_balance = balance_store.get_last(format!(
-                "{0}:{1}",
-                &pool_id,
-                hex::encode(&token0)
-            ));
+            // // Helper function to get balance at specific ordinal
+            let get_balance_at_ord = |key: &str, ord: u64| -> Option<BigInt> {
+                balances_map.get(key).and_then(|entries| {
+                    // Find the last entry with ord >= tx_ord
+                    entries
+                        .iter()
+                        .find(|(entry_ord, _)| *entry_ord >= ord)
+                        .or_else(|| entries.last())
+                        .map(|(_, balance)| balance.clone())
+                })
+            };
 
-            let token1_balance = balance_store.get_last(format!(
-                "{0}:{1}",
-                &pool_id,
-                hex::encode(&token1)
-            ));
-            //liquidity - the amount of lp tokens in the contract 
-            let liquidity_balance = balance_store.get_last(format!(
-                "{0}:{1}",
-                &pool_id,
-                &pool_id // pool and token address is the same
-            ));
- 
-            // 1. Construct the keys first
-            let key_token0 = format!("{}:{}", &pool_id, hex::encode(&token0));
-            let key_token1 = format!("{}:{}", &pool_id, hex::encode(&token1));
-            let key_liquidity = format!("{}:{}", &pool_id, &pool_id);
+            let key_token0 = format!("{}:{}", pool_id, hex::encode(&token0));
+            let key_token1 = format!("{}:{}", pool_id, hex::encode(&token1));
+            let key_liquidity = format!("{}:{}", pool_id, pool_id);
 
-            // 2. Log them
-            substreams::log::info!("Store Keys -> Token0: {}, Token1: {}, Liquidity: {}", key_token0, key_token1, key_liquidity);
-            substreams::log::info!(
-                "token 0 balance {:?}", token0_balance
-            );
-            
-            substreams::log::info!(
-                "token 1 balance {:?}",
-                token1_balance,
-            );
-            
-            substreams::log::info!(
-                "liquidity balance {:?}", liquidity_balance
-            );
+            let token0_balance = get_balance_at_ord(&key_token0, tx_ord);
+            let token1_balance = get_balance_at_ord(&key_token1, tx_ord);
+            let liquidity_balance = get_balance_at_ord(&key_liquidity, tx_ord);
 
             let pool_address_utf8 = pool
                 .id
@@ -357,18 +306,36 @@ fn merge_block(
 
     let trace_data = TraceData::Rpc(RpcTraceData {
         caller: None,
-        calldata: hex::decode("18160ddd").unwrap(), // totalSupply()
+        calldata: abi::pool::functions::TotalSupply {}.encode()
     });
 
     // let mut entrypoints = HashSet::new();
     // let mut entrypoint_params = HashSet::new();
 
+    //create entrypoints for all components 
     // Add created pools to the tx_changes_map
-    for change in block_entity_changes
+    for mut change in block_entity_changes
         .changes
         .clone()
         .into_iter()
     {
+        // Create entrypoints for each component before inserting into map
+        for component in change.component_changes.iter() {
+            //Get the totalSupply() of lp tokens using DCI entrypoint
+            let (entrypoint, entrypoint_params) = create_entrypoint(
+                component
+                    .id
+                    .clone()
+                    .as_bytes()
+                    .to_vec(),
+                "totalSupply()".to_string(),
+                component.id.clone(), //string 
+                trace_data.clone(),
+            );
+            change.entrypoints.push(entrypoint);
+            change.entrypoint_params.push(entrypoint_params);
+        }
+
         let transaction = change.tx.as_ref().unwrap();
         tx_entity_changes_map
             .entry(transaction.hash.clone())
@@ -377,21 +344,10 @@ fn merge_block(
                     .extend(change.component_changes.clone());
                 c.entity_changes
                     .extend(change.entity_changes.clone());
-                let _= change.component_changes.iter().for_each(|component| {
-                     //Get the totalSupply() of lp tokens using DCI entrypoint
-                    let (entrypoint, entrypoint_params) = create_entrypoint(
-                        component
-                            .id
-                            .clone()
-                            .as_bytes()
-                            .to_vec(),
-                        "totalSupply()".to_string(),
-                        component.id.clone(), //string 
-                        trace_data.clone(),
-                    );
-                    c.entrypoints.push(entrypoint);
-                    c.entrypoint_params.push(entrypoint_params);
-                });
+                c.entrypoints
+                    .extend(change.entrypoints.clone());
+                c.entrypoint_params
+                    .extend(change.entrypoint_params.clone());
             })
             .or_insert(change);
     }
